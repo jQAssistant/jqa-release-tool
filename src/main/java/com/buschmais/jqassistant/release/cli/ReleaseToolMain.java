78036d12a73305ee03ce7b6911bf3358b9ef5e92
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2017-03-21 21:08:43 +0100

    Initial commit.

---
+++ src/main/java/com/buschmais/jqassistant/release/cli/ReleaseToolMain.java
@@ -0,0 +1,19 @@
+package com.buschmais.jqassistant.release.cli;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.Bootstrap;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class ReleaseToolMain {
+    public static void main(String[] args) {
+
+        try {
+            Bootstrap.main(args);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
