41635b2933581bdcb036465070be2cb9a3a64c28
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2018-10-06 23:40:21 +0200

    [buschmais/jqa-release-tool#2] Overhauled the reporting of messages of the showversion command

--- commands/showcurrentversions/src/main/java/com/buschmais/jqassistant/release/showversions/ShowVersionsCommand.java
+++ commands/showcurrentversions/src/main/java/com/buschmais/jqassistant/release/showversions/ShowVersionsCommand.java
@@ -1,28 +1,26 @@
 package com.buschmais.jqassistant.release.showversions;
 
-import com.buschmais.jqassistant.release.core.ProjectRepository;
+import com.buschmais.jqassistant.release.core.RTExceptionWrapper;
 import com.buschmais.jqassistant.release.repository.RepositoryProviderService;
-import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.FileInputStream;
-import java.util.Set;
 
 import static java.lang.String.format;
+import static org.springframework.boot.ansi.AnsiColor.*;
+import static org.springframework.boot.ansi.AnsiColor.DEFAULT;
 
 @SpringBootApplication(scanBasePackages = {
     "com.buschmais.jqassistant.release.core",
     "com.buschmais.jqassistant.release.repository"
 })
-public class ShowVersionsCommand implements CommandLineRunner {
+public class ShowVersionsCommand implements ApplicationRunner {
 
-    RepositoryProviderService repositorySrv;
+    private RepositoryProviderService repositorySrv;
 
     public RepositoryProviderService getRepositorySrv() {
         return repositorySrv;
@@ -34,26 +32,33 @@ public class ShowVersionsCommand implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(ShowVersionsCommand.class);
+        var app = new SpringApplication(ShowVersionsCommand.class);
         app.setBannerMode(Banner.Mode.OFF);
-        ConfigurableApplicationContext run = app.run(args);
-        int exitCode = SpringApplication.exit(run);
+        var run = app.run(args);
+        var exitCode = SpringApplication.exit(run);
 
         System.exit(exitCode);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Set<ProjectRepository> projects = getRepositorySrv().getProjectRepositories();
+    public void run(ApplicationArguments __) throws Exception {
+        System.out.println(AnsiOutput.toString(BRIGHT_GREEN, "Showing the current version of all projects", DEFAULT));
 
-        for (ProjectRepository p : projects) {
-            var path = p.getHumanName() + "/pom.xml";
+        try {
+            var projects = getRepositorySrv().getProjectRepositories();
 
-            FileInputStream fis = new FileInputStream(path);
-            MavenXpp3Reader reader = new MavenXpp3Reader();
-            Model model = reader.read(fis);
-            String line = format("%-40s\t%s", model.getName(), model.getVersion());
-            System.out.println(line);
+            for (var project : projects) {
+                var path = project.getHumanName() + "/pom.xml";
+
+                try (var fis = new FileInputStream(path)) {
+                    var reader = new MavenXpp3Reader();
+                    var model = reader.read(fis);
+                    var line = format("%-40s\t%s", model.getName(), model.getVersion());
+                    System.out.println(line);
+                }
+            }
+        } catch (Exception e) {
+            throw RTExceptionWrapper.WRAPPER.apply(e, () -> "Failed to show the version of all projects.");
         }
     }
 }
