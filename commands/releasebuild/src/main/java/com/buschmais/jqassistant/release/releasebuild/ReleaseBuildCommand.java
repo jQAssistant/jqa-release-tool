c11ce66d7ee544af6ae83a673e1303b6d235c185
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2018-10-07 00:04:08 +0200

    [buschmais/jqa-release-tool#2] Overhauled the reporting of messages of the releasebuild command

--- commands/releasebuild/src/main/java/com/buschmais/jqassistant/release/releasebuild/ReleaseBuildCommand.java
+++ commands/releasebuild/src/main/java/com/buschmais/jqassistant/release/releasebuild/ReleaseBuildCommand.java
@@ -1,14 +1,11 @@
 package com.buschmais.jqassistant.release.releasebuild;
 
-import com.buschmais.jqassistant.release.core.ProjectRepository;
+import com.buschmais.jqassistant.release.core.RTExceptionWrapper;
 import com.buschmais.jqassistant.release.repository.RepositoryProviderService;
 import com.buschmais.jqassistant.release.services.maven.MavenRequest;
 import com.buschmais.jqassistant.release.services.maven.MavenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.*;
 import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,8 +15,10 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
+import static org.springframework.boot.ansi.AnsiColor.*;
+import static org.springframework.boot.ansi.AnsiColor.BRIGHT_GREEN;
+import static org.springframework.boot.ansi.AnsiColor.BRIGHT_RED;
 import static org.springframework.boot.ansi.AnsiStyle.BOLD;
 import static org.springframework.boot.ansi.AnsiStyle.NORMAL;
 
@@ -28,7 +27,7 @@ import static org.springframework.boot.ansi.AnsiStyle.NORMAL;
     "com.buschmais.jqassistant.release.repository",
     "com.buschmais.jqassistant.release.services.maven"
 })
-public class ReleaseBuildCommand implements CommandLineRunner {
+public class ReleaseBuildCommand implements ApplicationRunner {
 
     @Autowired
     private MavenService mavenService;
@@ -45,7 +44,7 @@ public class ReleaseBuildCommand implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(ReleaseBuildCommand.class);
+        var app = new SpringApplication(ReleaseBuildCommand.class);
         app.setBannerMode(Banner.Mode.OFF);
         ConfigurableApplicationContext run = app.run(args);
         int exitCode = SpringApplication.exit(run);
@@ -54,34 +53,34 @@ public class ReleaseBuildCommand implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Set<ProjectRepository> projects = getRepositorySrv().getProjectRepositories();
+    public void run(ApplicationArguments __) throws Exception {
+        var projects = getRepositorySrv().getProjectRepositories();
 
-        System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_GREEN, "Building jQA release with execution of unit tests and with a run of jQAssistant", AnsiColor.DEFAULT));
-        System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_GREEN, "Build artifacts will be uploaded to a staging repository at OSS Sonatype", AnsiColor.DEFAULT));
-        System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_RED, "Integration tests will not be executed at the moment.", AnsiColor.DEFAULT));
-        System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_RED, "jQAssistant constraints will not be executed at the moment.", AnsiColor.DEFAULT));
+        System.out.println(AnsiOutput.toString(BRIGHT_GREEN, "Building jQA release with execution of unit tests and with a run of jQAssistant", DEFAULT));
+        System.out.println(AnsiOutput.toString(BRIGHT_GREEN, "Build artifacts will be uploaded to a staging repository at OSS Sonatype", DEFAULT));
+        System.out.println(AnsiOutput.toString(BRIGHT_RED, "Integration tests will not be executed at the moment.", DEFAULT));
+        System.out.println(AnsiOutput.toString(BRIGHT_RED, "jQAssistant constraints will not be executed at the moment.", DEFAULT));
 
         try {
-            for (ProjectRepository p : projects) {
-                MavenRequest request = getMavenRequest(p.getHumanName());
+            for (var project : projects) {
+                MavenRequest request = getMavenRequest(project.getHumanName());
 
-                String s = AnsiOutput.toString(AnsiColor.BRIGHT_YELLOW,
+                String s = AnsiOutput.toString(BRIGHT_YELLOW,
                                                "About to run a release Maven build for ",
-                                               BOLD, AnsiColor.BRIGHT_YELLOW, "'",
-                                               p.getName(),
-                                               NORMAL, "'", AnsiColor.DEFAULT);
+                                               BOLD, BRIGHT_YELLOW, "'",
+                                               project.getName(),
+                                               NORMAL, "'", DEFAULT);
                 System.out.println(s);
 
                 mavenService.doRequest(request);
             }
-        } catch (Throwable t) {
-            t.printStackTrace();
+        } catch (Exception e) {
+            RTExceptionWrapper.WRAPPER.apply(e, () -> "Release build failed");
         }
     }
 
     protected MavenRequest getMavenRequest(String project) {
-        Properties properties = new Properties();
+        var properties = new Properties();
 
         try (var is = new FileInputStream("gpg.properties")) {
             properties.load(is);
@@ -93,8 +92,7 @@ public class ReleaseBuildCommand implements CommandLineRunner {
         var pp = properties.get("gpg.passphrase");
         var p1 = String.format("-Dgpg.keyname=%s", id);
         var p2 = String.format("-Dgpg.passphrase=%s", pp);
-
-        MavenRequest request = new MavenRequest();
+        var request = new MavenRequest();
 
         request.setGoals(Arrays.asList("clean", "deploy"));
         request.setParameters(List.of(p1, p2, "-Dmaven.test.failure.ignore=false",
