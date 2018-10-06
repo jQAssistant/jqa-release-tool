ec2004ba4dcb027c1511e53d30859ba635c86772
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2018-10-07 00:17:22 +0200

    [buschmais/jqa-release-tool#2] Overhauled the reporting of messages of the pushchanges command

--- commands/pushchanges/src/main/java/com/buschmais/jqassistant/release/pushchanges/PushChangesCommand.java
+++ commands/pushchanges/src/main/java/com/buschmais/jqassistant/release/pushchanges/PushChangesCommand.java
@@ -1,23 +1,25 @@
 package com.buschmais.jqassistant.release.pushchanges;
 
-import com.buschmais.jqassistant.release.core.ProjectRepository;
+import com.buschmais.jqassistant.release.core.RTExceptionWrapper;
 import com.buschmais.jqassistant.release.repository.RepositoryProviderService;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.transport.PushResult;
-import org.eclipse.jgit.transport.URIish;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.File;
 
-@SpringBootApplication(scanBasePackages = "com.buschmais.jqassistant.release")
-public class PushChangesCommand implements CommandLineRunner {
+import static org.springframework.boot.ansi.AnsiColor.*;
+import static org.springframework.boot.ansi.AnsiColor.BRIGHT_GREEN;
+import static org.springframework.boot.ansi.AnsiStyle.BOLD;
+import static org.springframework.boot.ansi.AnsiStyle.NORMAL;
 
-    RepositoryProviderService repositorySrv;
+@SpringBootApplication(scanBasePackages = "com.buschmais.jqassistant.release")
+public class PushChangesCommand implements ApplicationRunner {
+
+    private RepositoryProviderService repositorySrv;
 
     public RepositoryProviderService getRepositorySrv() {
         return repositorySrv;
@@ -29,33 +31,38 @@ public class PushChangesCommand implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(PushChangesCommand.class);
+        var app = new SpringApplication(PushChangesCommand.class);
         app.setBannerMode(Banner.Mode.OFF);
-        ConfigurableApplicationContext run = app.run(args);
-        int exitCode = SpringApplication.exit(run);
+        var run = app.run(args);
+        var exitCode = SpringApplication.exit(run);
 
         System.exit(exitCode);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        for (ProjectRepository projectRepository : getRepositorySrv().getProjectRepositories()) {
+    public void run(ApplicationArguments __) throws Exception {
+        System.out.println(AnsiOutput.toString(BRIGHT_GREEN, "Pushing all commits to the remote repositories.",
+                                               DEFAULT));
 
-            System.out.println(projectRepository.getHumanName());
+        var remote = "gh";
 
-            String remote = "gh";
-            String branch = "refs/heads/master";
-            String trackingBranch = "refs/remotes/" + remote + "/master";
+        try {
+            for (var repository : getRepositorySrv().getProjectRepositories()) {
+                var s = AnsiOutput.toString(BRIGHT_YELLOW, "Pushing changes of ",
+                                            BOLD, BRIGHT_YELLOW, "'", repository.getName(),
+                                            NORMAL, BRIGHT_YELLOW, "' to '", BOLD, remote, NORMAL,
+                                            "'", AnsiColor.DEFAULT);
 
-            Iterable<PushResult> gh = Git.open(new File(projectRepository.getHumanName()))
-                                         .push().setRemote(remote).setOutputStream(System.out)
-                                         .setPushAll()
-                                         .setPushTags()
-                                         .call();
+                System.out.println(s);
 
-            for (PushResult result : gh) {
-                System.out.println(result.getURI().getHumanishName());
+                var pushResult = Git.open(new File(repository.getHumanName()))
+                                    .push().setRemote(remote).setOutputStream(System.out)
+                                    .setPushAll()
+                                    .setPushTags()
+                                    .call();
             }
+        } catch (Exception e) {
+            RTExceptionWrapper.WRAPPER.apply(e, () -> "Failed to push all changes to " + remote);
         }
     }
 
