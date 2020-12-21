d062a3a51a17e810b1a85372563073e89d314f0a
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2020-12-21 14:45:00 +0100

    [jQAssistant/jqa-release-tool#4] Finished the implementation of the jqausages command

--- commands/jqausages/src/main/java/com/buschmais/jqassistant/release/jqausages/JQAUsagesCommand.java
+++ commands/jqausages/src/main/java/com/buschmais/jqassistant/release/jqausages/JQAUsagesCommand.java
@@ -3,9 +3,11 @@ package com.buschmais.jqassistant.release.jqausages;
 import com.buschmais.jqassistant.release.core.ProjectRepository;
 import com.buschmais.jqassistant.release.core.RTExceptionWrapper;
 import com.buschmais.jqassistant.release.repository.RepositoryProviderService;
-import org.apache.maven.model.Dependency;
-import org.apache.maven.model.DependencyManagement;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import one.util.streamex.StreamEx;
+import org.jboss.shrinkwrap.resolver.api.maven.*;
+import org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenDependency;
+import org.jboss.shrinkwrap.resolver.impl.maven.MavenWorkingSessionContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.ansi.AnsiOutput;
@@ -13,10 +15,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 import static java.lang.String.format;
 import static org.springframework.boot.ansi.AnsiColor.*;
@@ -31,7 +32,29 @@ import static org.springframework.boot.ansi.AnsiStyle.NORMAL;
 })
 public class JQAUsagesCommand implements ApplicationRunner {
 
+    private static String GROUP_ID_PREFIX = "com.buschmais.jqassistant";
+
     private RepositoryProviderService repositorySrv;
+
+    private Function<MavenDependency, String> toOutputFct = dependency -> {
+        var combined = dependency.getGroupId() + ":" + dependency.getArtifactId();
+        var version = dependency.getVersion();
+        var formatted = format("%-50s : %-10s", combined, version);
+
+        return formatted;
+    };
+
+    private Function<MavenDependency, Object> keyBuilderFct = dependency -> {
+        var groupId = dependency.getGroupId();
+        var artifactId = dependency.getArtifactId();
+        var version = dependency.getVersion();
+
+        return groupId + ":" + artifactId + ":" + version;
+    };
+
+    private Predicate<MavenDependency> isJQADependencyFct = dependency -> {
+        return dependency.getGroupId().startsWith(GROUP_ID_PREFIX);
+    };
 
     public RepositoryProviderService getRepositorySrv() {
         return repositorySrv;
@@ -53,46 +76,50 @@ public class JQAUsagesCommand implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments __) throws Exception {
+        String homeDir = System.getProperty("user.home");
+
         System.out.println(AnsiOutput.toString(BRIGHT_GREEN, "Showing the internal jQA dependencies of ",
                                                "each project", DEFAULT));
+        System.out.println(AnsiOutput.toString(BRIGHT_RED, "You must build everything before running this command, " +
+                                               "as the values are taken from the artifacts installed in the local " +
+                                               "Maven cache", DEFAULT));
 
         var projects = getRepositorySrv().getProjectRepositories();
 
         try {
-            for (ProjectRepository p : projects) {
+            for (ProjectRepository project : projects) {
                 var line = AnsiOutput.toString(BRIGHT_YELLOW, "About to run analyse dependencies of '",
-                                               BOLD, p.getName(),
+                                               BOLD, project.getName(),
                                                NORMAL, BRIGHT_YELLOW, "'",
                                                DEFAULT);
                 System.out.println(line);
 
-                var pomPath = p.getHumanName() + "/pom.xml";
+                var pomPath = project.getHumanName() + "/pom.xml";
                 var pomFile = new File(pomPath);
 
-                try (var fis = new FileInputStream(pomFile)) {
-                    var mavenReader = new MavenXpp3Reader();
-                    var model = mavenReader.read(fis);
-                    List<List<Dependency>> dod = new LinkedList<>();
+                MavenStrategyStage resolve = Maven.configureResolver()
+                                                  .workOffline()
+                                                  .fromFile(homeDir + "/jqa-release-environment/maven-settings.xml")
+                                                  .loadPomFromFile(pomFile)
+                                                  .importCompileAndRuntimeDependencies()
+                                                  .importRuntimeAndTestDependencies()
+                                                  .resolve();
 
-                    var dependencies = Optional.ofNullable(model.getDependencyManagement())
-                                                            .orElseGet(DependencyManagement::new)
-                                                            .getDependencies();
-                    var dependencies1 = Optional.ofNullable(model.getDependencies())
-                                                             .orElseGet(Collections::emptyList);
+                MavenWorkingSession mavenWorkingSession = ((MavenWorkingSessionContainer) resolve).getMavenWorkingSession();
 
-                    dod.add(dependencies);
-                    dod.add(dependencies1);
+                List<MavenDependency> dependencies = new ArrayList<>();
+                dependencies.addAll(mavenWorkingSession.getDependenciesForResolution());
+                dependencies.addAll(mavenWorkingSession.getDependencyManagement());
 
-                    dod.stream()
-                       .flatMap((Function<List<Dependency>, Stream<Dependency>>) Collection::stream)
-                       .filter(d -> d.getGroupId().startsWith("com.buschmais.jqassistant"))
-                       .forEach(o -> {
-                           var coord = format("%s:%s:%s", o.getGroupId(), o.getArtifactId(), o.getVersion());
-                           System.out.println(coord);
-                       });
-                }
+                StreamEx.of(dependencies)
+                           .distinct(keyBuilderFct)
+                           .filter(isJQADependencyFct)
+                           .map(toOutputFct)
+                           .sorted()
+                           .forEach(System.out::println);
             }
         } catch (RuntimeException e) {
+            e.printStackTrace();
             RTExceptionWrapper.WRAPPER.apply(e, () -> "Failed to show internal jQA dependencies.");
         }
     }
